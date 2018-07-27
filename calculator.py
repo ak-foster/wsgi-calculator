9251278303fc486bf8cdd6bb63ef1e45e2ec00c3
Andrew Foster <akfoster@uw.edu>
2018-07-26 23:19:58 -0700

    Tests passed. Pushing to master.

--- calculator.py
+++ calculator.py
@@ -1,57 +1,14 @@
-"""
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
-"""
+#!/usr/bin/env python3
 
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
-    sum = sum(args)
+    add_total = sum(args)
 
-    return str(sum)
-
-# Added functions for handling more arithmetic operations.
+    return str(add_total)
 
 
-def subract(*args):
+def subtracting(*args):
     total = args[0]
 
     for num in args[1:]:
@@ -77,27 +34,23 @@ def divide(*args):
 
     return str(quotient)
 
+def index(*args):
+    return "Use the address bar to do math.  E.g. .../subtract/10/2 will return 8"
 
 def resolve_path(path):
     """
-    Should return two values: a callable and an iterable of
-    arguments.
+    Return two values: a callable and an iterable of arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-
     funcs = {
+        '': index,
         'add': add,
-        'substract': subtract,
+        'subtract': subtracting,
         'multiply': multiply,
         'divide': divide,
     }
 
     path = path.strip('/').split('/')
-
     func_name = path[0]
     args = path[1:]
 
@@ -108,6 +61,7 @@ def resolve_path(path):
 
     return func, args
 
+
 def application(environ, start_response):
 
     headers = [("Content-type", "text/html")]
@@ -116,23 +70,22 @@ def application(environ, start_response):
         if path is None:
             raise NameError
         func, args = resolve_path(path)
-        body = func(*args)
+        i_args = [int(i) for i in args]
+        body = func(*i_args)
         status = "200 OK"
     except NameError:
         status = "404 Not Found"
-        body = "<h1>Not Found</h1>"
+        body = f"<h1>Not Found for {path}</h1>"
     except Exception:
         status = "500 Internal Server Error"
-        body = "<h1>Internal Server Error</h1>"
+        body = f"<h1>Internal Server Error for {path}</h1>"
         print(traceback.format_exc())
     finally:
         headers.append(('Content-length', str(len(body))))
         start_response(status, headers)
         return [body.encode('utf8')]
-        return [body]
 
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
+    # TODO: Add error handling for a user attempting to divide by zero.
 
 
 if __name__ == '__main__':
